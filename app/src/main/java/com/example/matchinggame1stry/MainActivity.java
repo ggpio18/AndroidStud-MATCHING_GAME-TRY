4fd09ce041f1a903127969bf0acb9f2a90ed56e6
ggpio18 <58065866+ggpio18@users.noreply.github.com>
2024-06-15 12:32:21 +0800

    firstcommit

---
+++ app/src/main/java/com/example/matchinggame1stry/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.matchinggame1stry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //btn
+        MaterialButton startbtn =(MaterialButton) findViewById(R.id.Startbtn);
+
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
